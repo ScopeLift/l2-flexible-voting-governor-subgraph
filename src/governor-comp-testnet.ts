547892a4de0c9dfe5695f817c008193aeb601732
Keating <keating.dev@protonmail.com>
2023-12-13 10:39:44 -0500

    Add initial subgraph

--- src/governor-comp-testnet.ts
+++ src/governor-comp-testnet.ts
@@ -36,7 +36,8 @@ export function handleProposalCreated(event: ProposalCreatedEvent): void {
   )
   entity.proposalId = event.params.proposalId
   entity.proposer = event.params.proposer
-  entity.targets = event.params.targets
+	// Weird type issue
+  // entity.targets = event.params.targets
   entity.values = event.params.values
   entity.signatures = event.params.signatures
   entity.calldatas = event.params.calldatas
